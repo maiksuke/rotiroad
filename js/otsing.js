515e4df36048cf39d0a14baa66a6040ff5eda891
maiksuke <maironm07@gmail.com>
2024-11-21 01:49:11 +0200

    fix otsingule

--- js/otsing.js
+++ js/otsing.js
@@ -3,29 +3,26 @@ function otsiRetsept(event) {
     const otsinguInput = document
         .getElementById("searchInput")
         .value.toLowerCase()
-    const kategooriad = document.querySelectorAll(".kategooria") // kõik kategooriad
-
+    const kategooriad = document.querySelectorAll(".kategooria") // Kõik kategooriad
     kategooriad.forEach(kategooria => {
-        const kaardid = kategooria.querySelectorAll(".kaart") // kategooria sees olevad kaardid
+        const kaardid = kategooria.querySelectorAll(".kaart") // Kategooria sees olevad kaardid
         let onKaardidNahtaval = false
-
         kaardid.forEach(kaart => {
             const retseptPealkiri = kaart
                 .querySelector(".kaart-pealkiri")
                 .textContent.toLowerCase()
-
             if (retseptPealkiri.includes(otsinguInput)) {
-                kaart.style.display = "" // näitab kaardi
+                kaart.style.display = "" // Kuvab kaardi
                 onKaardidNahtaval = true
             } else {
-                kaart.style.display = "none" // peidab kaardi
+                kaart.style.display = "none" // Peidab kaardi
             }
         })
-
+        // Peida või kuva kategooria sõltuvalt nähtavatest kaartidest
         if (onKaardidNahtaval) {
-            kategooria.classList.remove("hidden") // näitab kategooria
+            kategooria.classList.remove("hidden") // Kuvab kategooria
         } else {
-            kategooria.classList.add("hidden") // peidab kategooria
+            kategooria.classList.add("hidden") // Peidab kategooria
         }
     })
 }
